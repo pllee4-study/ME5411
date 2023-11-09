d79de64b960e23b8a91f83d98d6800889ed8f95b
Pin Loon Lee <pinloon_0428@hotmail.com>
2023-11-09 21:42:45 +0800

    scripts: part1: ...ImagePreProcessingTuningPlot.m: added more support
    Added support for both tuning and final cropping of image

--- scripts/part1/plotter/ImagePreProcessingTuningPlot.m
+++ scripts/part1/plotter/ImagePreProcessingTuningPlot.m
@@ -37,13 +37,16 @@ classdef ImagePreProcessingTuningPlot
                     ImagePreProcessingTuningPlot.plotDetail(5, 4, 16, figures, name, true);
 
                 case "ErodedImage"
-                    ImagePreProcessingTuningPlot.plotDetail(5, 4, 17, figures, name, true);
+                    ImagePreProcessingTuningPlot.plotDetail(5, 4, 17, figures, name, true, true);
                 otherwise
             end 
         end
 
-        function plotDetail(row, column, index, figures, name, showSegmentedCharacter)
-            if (showSegmentedCharacter)
+        function plotDetail(row, column, index, figures, name, showSegmentedCharacter, extraProcessing)
+            if nargin < 7 || isempty(extraProcessing)
+                extraProcessing = false;
+            end
+            if showSegmentedCharacter
                 cc = bwconncomp(figures, 4);
                 props = regionprops(cc, 'BoundingBox');
                 subplot(row, column, index), imshow(figures), title(name);
@@ -52,46 +55,50 @@ classdef ImagePreProcessingTuningPlot
                 coloredLabels = label2rgb(labelMatrix, turbo(numCc), 'k', 'shuffle');
                 imshow(coloredLabels);
                 hold on;
-
-                % Initialize the min width with a value larger than any rectangle width
-                minWidth = Inf;
-
-                % Find the smallest rectangle width
-                for i = 1:numel(props)
-                    width = props(i).BoundingBox(3);
-                    if width < minWidth
-                        minWidth = width;
+                if ~extraProcessing
+                    for i = 1:numel(props)
+                        rectangle('Position', props(i).BoundingBox, 'EdgeColor', 'r', 'LineWidth', 2);
                     end
-                end
+                else
+                    % Initialize the min width with a value larger than any rectangle width
+                    minWidth = Inf;
 
-                % Perform additional split if the rectangle width is significantly larger than the min width
-                splitThreshold = 1.7;  % Threshold to determine if the width is significantly larger
-         
-                for i = 1:numel(props)
-                    rectangle('Position', props(i).BoundingBox, 'EdgeColor', 'r', 'LineWidth', 2);
-
-                    % Check if the width is significantly larger than the minWidth
-                    width = props(i).BoundingBox(3);
-                    if width > splitThreshold * minWidth
-                        % Split the rectangle into two equal-width rectangles
-                        halfWidth = width / 2;
-                        x = int32(props(i).BoundingBox(1));
-                        y = int32(props(i).BoundingBox(2));
-                        height = int32(props(i).BoundingBox(4));
-
-                        % Create two new rectangles
-                        rect1 = [x, y, halfWidth, height];
-                        rect2 = [x + halfWidth, y, halfWidth, height];
-                        
-                        coloredLabels(y : y + height, x : x + halfWidth, 1) = 10;
-    
-                        % Draw the additional rectangles
-                        rectangle('Position', rect1, 'EdgeColor', 'g', 'LineWidth', 2);
-                        rectangle('Position', rect2, 'EdgeColor', 'b', 'LineWidth', 2);    
+                    % Find the smallest rectangle width
+                    for i = 1:numel(props)
+                        width = props(i).BoundingBox(3);
+                        if width < minWidth
+                            minWidth = width;
+                        end
                     end
+
+                    % Perform additional split if the rectangle width is significantly larger than the min width
+                    splitThreshold = 1.7;  % Threshold to determine if the width is significantly larger
+                    for i = 1:numel(props)
+                        rectangle('Position', props(i).BoundingBox, 'EdgeColor', 'r', 'LineWidth', 2);
+
+                        % Check if the width is significantly larger than the minWidth
+                        width = props(i).BoundingBox(3);
+                        if width > splitThreshold * minWidth
+                            % Split the rectangle into two equal-width rectangles
+                            halfWidth = width / 2;
+                            x = int32(props(i).BoundingBox(1));
+                            y = int32(props(i).BoundingBox(2));
+                            height = int32(props(i).BoundingBox(4));
+
+                            % Create two new rectangles
+                            rect1 = [x, y, halfWidth, height];
+                            rect2 = [x + halfWidth, y, halfWidth, height];
+                            
+                            coloredLabels(y : y + height, x : x + halfWidth, 1) = 10;
+        
+                            % Draw the additional rectangles
+                            rectangle('Position', rect1, 'EdgeColor', 'g', 'LineWidth', 2);
+                            rectangle('Position', rect2, 'EdgeColor', 'b', 'LineWidth', 2);    
+                        end
+                    end
+                    % show the processed coloredLabels
+                    imshow(coloredLabels);
                 end
-                % show the processed coloredLabels
-                imshow(coloredLabels);
             else
                 subplot(row, column, index), imshow(figures), title(name);
             end
