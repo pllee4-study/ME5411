9fdf73ebbd4029e15c79d0da9ae33c8a6fb288fe
Pin Loon Lee <pinloon_0428@hotmail.com>
2023-11-12 08:03:29 +0800

    part1.m: clean up from octave and updated rotating mask

--- scripts/part1.m
+++ scripts/part1.m
@@ -1,34 +1,20 @@
-if exist('OCTAVE_VERSION', 'builtin') == 5
-    % Code specific to Octave
-    disp('Running in Octave');
-    runInMatlab = false;
-    pkg load image;
-else
-    % Code specific to MATLAB
-    disp('Running in MATLAB');  
-    runInMatlab = true;
-end
-
-if ~runInMatlab
-    source('./utility/loadImageFromLocalOrOnline.m');
-    source('./part1/rgb2gray.m');
-else
-    scriptPath = fileparts(mfilename('fullpath'));
-    rootPath = fileparts(scriptPath);
-    run(fullfile(rootPath, 'scripts/utility/loadImageFromLocalOrOnline.m'));
-end
+% Run script to load image
+scriptPath = fileparts(mfilename('fullpath'));
+rootPath = fileparts(scriptPath);
+run(fullfile(rootPath, 'scripts/utility/loadImageFromLocalOrOnline.m'));
+addpath(fullfile(rootPath, 'scripts/part1'));
 
 % Read the image
 img = imageLoaded;
 
 % Create a structuring element
-se = strel('disk', 8, 0);  % Create a disk-shaped structuring element with radius 8 pixels
+se = strel('disk', 6);  % Create a disk-shaped structuring element with radius 6 pixels
 
 % Perform image filtering using the structuring element
 openedImage = imopen(img, se);  % Perform opening operation
 
 % Convert the image to grayscale
-imgGrayScale = rgb2gray(openedImage);
+imgGrayScale = rgb2gray(img);
 
 % Define the 3x3 and 5x5 averaging mask
 threeByThreeAverageMask = ones(3) / 9;
@@ -39,8 +25,10 @@ threeByThreeImgAverage = conv2(double(imgGrayScale), threeByThreeAverageMask, 's
 fiveByFiveImgAverage = conv2(double(imgGrayScale), fiveByFiveAverageMask, 'same');
 
 % Define the 3x3 and 5x5 rotating mask
-threeByThreeRotMask = [0 1 0; 1 -4 1; 0 1 0];
-fiveByFiveRotMask = [-1 -1 -1 -1 -1; -1 1 2 1 -1; -1 2 4 2 -1; -1 1 2 1 -1; -1 -1 -1 -1 -1];
+% threeByThreeRotMask = [3 5 3; 5 8 5; 3 5 3] / 200;
+% fiveByFiveRotMask = [5 6 9 6 5; 6 7 11 7 6; 9 11 16 11 9; 6 7 11 7 6; 5 6 9 6 5] / 1648;
+threeByThreeRotMask = [3 5 3; 5 8 5; 3 5 3] / 50;
+fiveByFiveRotMask = [5 6 9 6 5; 6 7 11 7 6; 9 11 16 11 9; 6 7 11 7 6; 5 6 9 6 5] / 412;
 
 % Apply the rotating mask using the conv2 function
 threeByThreeImgRotate = conv2(double(imgGrayScale), threeByThreeRotMask, 'same');
@@ -56,13 +44,6 @@ outlineImage = dilatedImage - erodedImage;
 figure('Name', 'ME5411 Group 11');
 subplot(4, 3, 1), imshow(img), title('Original Image');
 subplot(4, 3, 2), imshow(openedImage), title('Opened Image');
-
-if ~runInMatlab
-  filtered_image = imsmooth(rgb2gray(img), 'Disk');
-else
-  filtered_image = imgaussfilt(rgb2gray(img), 2);
-end
-
 subplot(4, 3, 3), imshow(closedImage), title('Closed Image');
 subplot(4, 3, 4), imshow(uint8(threeByThreeImgAverage)), title('Averaging Mask(3x3)');
 subplot(4, 3, 5), imshow(uint8(threeByThreeImgRotate)), title('Rotating Mask(3x3)');
@@ -73,16 +54,13 @@ subplot(4, 3, 9), imshow(erodedImage), title('Eroded Image');
 subplot(4, 3, 12), imshow(outlineImage), title('Outline Image');
 
 [rows, cols, ~] = size(uint8(threeByThreeImgAverage));
-subImage = openedImage(rows/2+1:rows, :, :);
+startingRow = (rows / 2) + 1;
+subImage = img(int32(startingRow) : int32(rows), :, :);
 
 grayScaleSubImage = rgb2gray(subImage);
 
 % Convert the grayscale image to binary
-if ~runInMatlab
-    binarySubImage = im2bw(grayScaleSubImage, 0.35079); %0.3615
-else
-    binarySubImage = imbinarize(grayScaleSubImage, 'adaptive');
-end
+binarySubImage = imbinarize(grayScaleSubImage, 0.3852);
 
 subplot(4, 3, 10), imshow(subImage), title('SubImage');
 subplot(4, 3, 11), imshow(binarySubImage), title('Binary SubImage');
@@ -99,15 +77,13 @@ sliderHeight = 0.05 * figHeight;  % 5% of the figure height
 slider = uicontrol('style', 'slider', 'position', [sliderLeft, sliderBottom, sliderWidth, sliderHeight], 'callback', @(src, event) updateThreshold(src, grayScaleSubImage));
 set(slider, 'Min', 0, 'Max', 1, 'Value', 0.5, 'SliderStep', [0.01 0.1], 'UserData', binarySubImage, 'Tag', 'ThresholdSlider');
 
-if runInMatlab
-    addlistener(slider, 'ContinuousValueChange', @(src, event) updateThreshold(src, grayScaleSubImage));
-end
+addlistener(slider, 'ContinuousValueChange', @(src, event) updateThreshold(src, grayScaleSubImage));
 
 function updateThreshold(sliderValue, grayScaleSubImage)
     threshold = get(sliderValue, 'Value');
     disp(threshold);
     try
-        binarySubImage = im2bw(grayScaleSubImage, threshold);
+        binarySubImage = imbinarize(grayScaleSubImage, threshold);
     catch ME
         error('Failed to convert the image to binary: %s', ME.message);
     end
