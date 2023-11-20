6d306900119c3d9d9fc02645f2c1e72696506bea
Pin Loon Lee <pinloon_0428@hotmail.com>
2023-11-20 22:21:07 +0800

    scripts: RecognitionWithTrainedCNN.m: display result

--- scripts/RecognitionWithTrainedCNN.m
+++ scripts/RecognitionWithTrainedCNN.m
@@ -14,5 +14,22 @@ for i = 1:length(imagefiles)
     currentImage = imread(imgPath);
     grayImage = im2gray(currentImage);
     [label, scores] = classify(net, grayImage);
-    fprintf('Element %d: Predicted label: %s, Score: %f\n', i, char(label), max(scores));
+    results(i).filename = imgPath;
+    results(i).original_label = extractBetween(imagefiles(i).name, 'label_', '_');
+    results(i).predicted_label = char(label);
+    results(i).scores = scores;
+fprintf('Element %d: Predicted label: %s, Score: %f\n', i, char(label), max(scores));
+end
+%% 
+% 对结果按照文件名排序
+[~, order] = sort({imagefiles.name});
+results = results(order);
+% 可视化
+figure('Name', 'Image Classification Results');
+
+nfiles = length(imagefiles);
+for i = 1:nfiles
+    subplot(2, ceil(nfiles/2), i);
+    imshow(imread(results(i).filename));
+    title(sprintf(' \nCNNPredicted: %s', results(i).original_label{:}, results(i).predicted_label));
 end
